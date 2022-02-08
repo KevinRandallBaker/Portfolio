663f3c13162687f7b28e75e1fb102a3360612bdc
KevinRandallBaker <kevinrandallbaker@gmail.com>
2022-02-07 22:27:27 -0800

    initial commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import "./topbar.scss";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">dsfdsfs</a>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+}
