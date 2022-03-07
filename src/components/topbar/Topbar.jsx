141f10e027bf16eb358fc8d5dd0f0ac3402428a9
Kevin Baker <70351842+KevinRandallBaker@users.noreply.github.com>
2022-03-07 13:51:12 -0800

    Fixed issue where containers were not in the right place wihtin topbar.scss

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,25 +1,25 @@
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar active">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            genius.
+            Kevin Baker
           </a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+44 924 12 74</span>
+            <span>714-329-0350</span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>johnsmith@foo.com</span>
+            <span>kevinrandallbaker@gmail.com</span>
           </div>
         </div>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
