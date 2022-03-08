4865e637e7614854ec3145887ed0c59a490a9d5b
Kevin Baker <70351842+KevinRandallBaker@users.noreply.github.com>
2022-03-08 00:25:27 -0800

    Begin flushing out Portfolio. Currently fixing the items in the container.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,60 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/719/1*WaaXnUvhvrswhBJSw4YTuQ.png"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/719/1*WaaXnUvhvrswhBJSw4YTuQ.png"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/719/1*WaaXnUvhvrswhBJSw4YTuQ.png"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/719/1*WaaXnUvhvrswhBJSw4YTuQ.png"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/719/1*WaaXnUvhvrswhBJSw4YTuQ.png"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/719/1*WaaXnUvhvrswhBJSw4YTuQ.png"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
