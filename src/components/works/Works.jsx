b0012408c9d955feee65197abd31645205940478
Kevin Baker <70351842+KevinRandallBaker@users.noreply.github.com>
2022-03-27 12:49:58 -0700

    Fixed arrow keys. Was one errant curly bracket within the slider declaration.

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -37,7 +37,7 @@ export default function Works() {
     <div className="works" id="works">
       <div
         className="slider"
-        style={{ transform: `translateX(-${currentSlide * 100}vw})` }}
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
       >
         {data.map((d) => (
           <div className="container">
